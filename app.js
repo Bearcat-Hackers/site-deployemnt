daf4ae923a0cc936b7168e7ba608de6384b1115b
Chris Crowell <crowelch@mail.uc.edu>
2015-07-29 22:32:53 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+var gith = require('gith').create(8010);
+var shell = require('shelljs');
+
+
+gith({
+	repo: 'Bearcat-Hackers/RevolutionUC-Website'
+}).on('all', function(paylod) {
+	if(paylod.branch === 'master') {
+
+	}
+})
+
+function deploy() {
+	shell.cd('/home/curtis/RevolutionUC-F2014/RevolutionUC-Website/');
+	shell.exec('git pull https://github.com/Bearcat-Hackers/RevolutionUC-Website.git');
+	shell.exec('grunt build');
+}
