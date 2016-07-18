cbf59e420ef931ce6a5e1327ed80a8461789d8df
Dom Farolino <domfarolino@gmail.com>
2016-07-18 12:50:50 -0400

    fix(): update to work with new build

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ var PORT = 8010
 webhooks.on('push', function(payload) {
 		console.log('about to deploy!');
 
-		shell.cd('/home/curtis/RevolutionUC-F2014/RevolutionUC-Website/');
-		shell.exec('git pull https://github.com/Bearcat-Hackers/RevolutionUC-Website.git');
+		shell.cd('/home/dom/RevolutionUC-Website/');
+		shell.exec('git pull');
 		shell.exec('grunt build');
 
 		console.log('done building!');
